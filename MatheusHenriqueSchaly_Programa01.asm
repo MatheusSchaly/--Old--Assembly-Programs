0f6bfb2f7d9877e647f7d2fc33e05342aed82feb
Matheus Schaly <matheusmhs@hotmail.com>
2017-09-09 12:49:41 -0300

    1.2 Iniciando Trabalho 1

---
+++ MatheusHenriqueSchaly_Programa01.asm
@@ -0,0 +1,9 @@
+# Disciplina: Arquitetura e Organização de Computadores
+# Atividade: Avaliação 01 – Programação em Linguagem de Montagem
+# Programa 01
+# Grupo: - Matheus Henrique Schaly
+
+.data
+	vetor1: .word 0, 0, 0, 0, 0, 0, 0, 0	#Inicializando vetor 1
+	vetor2: .word 0, 0, 0, 0, 0, 0, 0, 0	#Inicializando vetor 2
+.text
